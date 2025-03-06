af97dcc6c7d320fc376bbb5b4d5ef4ab8b3eefc5
KgMoney38 <kodydavison@hotmail.com>
2025-03-05 19:19:35 -0600

    Data Item done

--- src/DataItem.java
+++ src/DataItem.java
@@ -1,2 +1,28 @@
-public class DataItem {
+public class DataItem
+{
+    private String itemName;
+    private double itemValue;
+    private int year;
+
+    public DataItem(String itemName, double itemValue, int year)
+    {
+        this.itemName = itemName;
+        this.itemValue = itemValue;
+        this.year = year;
+    }
+
+    public String getItemName()
+    {
+        return itemName;
+    }
+
+    public double getItemValue()
+    {
+        return itemValue;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
 }
