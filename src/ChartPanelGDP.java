6890e6c182fca2e5bba26dee8a9ef19e8ed976b1
KgMoney38 <kodydavison@hotmail.com>
2025-03-16 22:45:14 -0500

    Attempt add multi colored lines for filter of countries

--- src/ChartPanelGDP.java
+++ src/ChartPanelGDP.java
@@ -8,6 +8,7 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ChartPanelGDP extends JPanel
@@ -34,15 +35,30 @@ public class ChartPanelGDP extends JPanel
         add(chartPanel, BorderLayout.CENTER);
     }
 
-    public void updateChart(List<DataItem> dataItems)
-    {
-     dataset.clear();
-     dataItems.stream()
-             .collect(Collectors.groupingBy(DataItem::getYear))
-             .forEach((year,list) -> {
-                 double totalGDP = list.stream().mapToDouble(DataItem::getGDP).sum();
-                 dataset.addValue(totalGDP, "Total GDP", String.valueOf(year));
-             });
+    public void updateChart(List<DataItem> dataItems) {
+        dataset.clear();
+
+        //Add multi colored lines for multiple countries selected
+        Map<String, Map<Integer, Double>> countryYearGDPMap = dataItems.stream()
+                .collect(Collectors.groupingBy(DataItem::getCountry,
+                        Collectors.groupingBy(DataItem::getYear,
+                                Collectors.summingDouble(DataItem::getGDP))));
+
+        int colorIndex = 0;
+        String[] colors = {"#FF0000", "#0000FF", "#008000", "#FFA500", "#800080"};
+
+        for (String country : countryYearGDPMap.keySet()) {
+            Map<Integer, Double> yearlyGDP = countryYearGDPMap.get(country);
+
+            for (Map.Entry<Integer, Double> entry : yearlyGDP.entrySet()) {
+                dataset.addValue(entry.getValue(), country, String.valueOf(entry.getKey()));
+            }
+
+            if (colorIndex < colors.length) {
+                chart.getCategoryPlot().getRenderer().setSeriesPaint(colorIndex, Color.decode(colors[colorIndex]));
+                colorIndex++;
+            }
+        }
     }
 }
 
